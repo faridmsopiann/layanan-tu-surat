a250694b688aab1d52b83a258029e3aa2931fae1
Farid Muhammad S <faridsopian08@gmail.com>
2025-05-12 19:06:57 +0700

    fix login register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -89,6 +89,9 @@
     });
 </script>
 
+<!-- Tambahkan ini di dalam <head> -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap');
     
